67f90e639c9583bf3e1c4ae52d23137589b1dce8
VijaySJ <vijaysjofficial@gmail.com>
2025-07-31 17:59:16 +0530

    Login Test minor changes is done in the 14th line of code

--- Orange/src/test/java/tests/LoginTest.java
+++ Orange/src/test/java/tests/LoginTest.java
@@ -7,13 +7,10 @@ import org.testng.annotations.Test;
 import dataProviders.ExcelDataProvider;
 import pages.LoginPage;
 
-public class LoginTest extends BaseTest{
+public class LoginTest extends BaseTest {
 
-	@Test(dataProvider= "loginData", dataProviderClass = ExcelDataProvider.class)
+	@Test(dataProvider = "loginData", dataProviderClass = ExcelDataProvider.class)
 	public void login(Map<String, String> data) {
-		new LoginPage()
-		.enterUsername(data.get("username"))
-		.enterPassword (data.get("password"))
-		.clickLogin();
+		new LoginPage().enterUsername(data.get("username")).enterPassword(data.get("password")).clickLogin();
 	}
 }
